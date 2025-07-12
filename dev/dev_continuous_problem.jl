bf5ed819475a9a160088e179e5e4810bf5c60c50
Yuri <yuri.shimane@gmail.com>
2025-07-12 00:18:51 -0400

    example plot update

--- dev/dev_continuous_problem.jl
+++ dev/dev_continuous_problem.jl
@@ -75,8 +75,12 @@ end
 function eom_aug!(dx_aug, x_aug, p, t)
     x, y, z = x_aug[1:3]
     vx, vy, vz = x_aug[4:6]
-    r1 = sqrt( (x+p.μ)^2 + y^2 + z^2 );
-    r2 = sqrt( (x-1+p.μ)^2 + y^2 + z^2 );
+
+    r1vec = [x + p.μ, y, z]
+    r2vec = [x - 1 + p.μ, y, z]
+    r1 = norm(r1vec)
+    r2 = norm(r2vec)
+
     dx_aug[1:3] = x_aug[4:6]
     # derivatives of velocities
     dx_aug[4] =  2*vy + x - ((1-p.μ)/r1^3)*(p.μ+x) + (p.μ/r2^3)*(1-p.μ-x);
@@ -86,23 +90,17 @@ function eom_aug!(dx_aug, x_aug, p, t)
     # append controls
     dx_aug[4:6] += p.u[1:3]
     
-    # ForwardDiff.jacobian!(A, (y,x) -> eom!(y, x, p, t), dx_aug[1:6], x_aug[1:6])
-    A = ForwardDiff.jacobian(x -> eom(x, p, t), x_aug[1:6])
-    # A = cr3bp_dfdx(x_aug[1:6], p)
-    #, cfg::JacobianConfig = JacobianConfig(eom!, dx_aug[1:6], x_aug[1:6]), check=Val{true}())
-    # A = ForwardDiff.jacobian(x -> eom!(zeros(6), x, params, t), x_aug[1:6])
-
-    # A = sparse_jacobian(adtype, cache_jac_A, (y,x) -> eom!(y, x, p, t), dx_aug[1:6], x_aug[1:6])
-    # A = sparse_jacobian(adtype, cache_jac_A, (y,x) -> eom!(y, x, p, t), similar(x_aug[1:6]), x_aug[1:6])
+    # Jacobian derivatives
+    G1 = (1 - params.μ) / norm(r1vec)^5*(3*r1vec*r1vec' - norm(r1vec)^2*I(3))
+    G2 = params.μ / norm(r2vec)^5*(3*r2vec*r2vec' - norm(r2vec)^2*I(3))
+    Omega = [0 2 0; -2 0 0; 0 0 0]
+    A = [zeros(3,3)                  I(3);
+         G1 + G2 + diagm([1,1,0])    Omega]
     B = [zeros(3,4); I(3) zeros(3,1)]
-    
-    # extract STMs (note: julia is column-major)
-    #Phi_A = reshape(x_aug[nx+1:nx*(nx+1)], (nx,nx))'
-    Phi_B = reshape(x_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu], (nu,nx))'  # note: julia is column-major
 
     # derivatives of Phi_A, Phi_B
     dx_aug[7:42] = reshape((A * reshape(x_aug[7:42],6,6)')', 36)
-    dx_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu] = reshape((A * Phi_B + B)', nx*nu)
+    dx_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu] = reshape((A * reshape(x_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu], (nu,nx))' + B)', nx*nu)
 end
 
 
@@ -154,7 +152,7 @@ u_ref = zeros(nu, N-1)
 y_ref = nothing
 
 # plot initial guess
-fig = Figure(size=(1200,400))
+fig = Figure(size=(1200,800))
 ax3d = Axis3(fig[1,1]; aspect=:data)
 lines!(Array(sol_lpo0)[1,:], Array(sol_lpo0)[2,:], Array(sol_lpo0)[3,:], color=:blue)
 lines!(Array(sol_lpof)[1,:], Array(sol_lpof)[2,:], Array(sol_lpof)[3,:], color=:green)
@@ -214,7 +212,7 @@ for (i, _sol) in enumerate(sols_opt)
 end
 
 # plot controls
-ax_u = Axis(fig[1,2]; xlabel="Time", ylabel="Control")
+ax_u = Axis(fig[2,1]; xlabel="Time", ylabel="Control")
 for i in 1:3
     stairs!(ax_u, prob.times[1:end-1], solution.u[i,:], label="u[$i]", step=:pre, linewidth=1.0)
 end
@@ -223,12 +221,17 @@ axislegend(ax_u, position=:cc)
 
 # plot iterate information
 colors_accept = [solution.info[:accept][i] ? :green : :red for i in 1:length(solution.info[:accept])] 
-ax_χ = Axis(fig[1,3]; xlabel="Iteration", ylabel="χ", yscale=log10)
+ax_χ = Axis(fig[1,2]; xlabel="Iteration", ylabel="χ", yscale=log10)
 scatterlines!(ax_χ, 1:length(solution.info[:accept]), solution.info[:χ], color=colors_accept, marker=:circle, markersize=7)
-# ax_w = Axis(fig[2,3]; xlabel="Iteration", ylabel="w", yscale=log10)
-# scatterlines!(ax_w, 1:length(solution.info[:accept]), solution.info[:w], color=colors_accept, marker=:circle, markersize=7)
-# ax_Δ = Axis(fig[3,3]; xlabel="Iteration", ylabel="trust region radius", yscale=log10)
-# scatterlines!(ax_Δ, 1:length(solution.info[:accept]), solution.info[:Δ], color=colors_accept, marker=:circle, markersize=7)
+
+ax_w = Axis(fig[2,2]; xlabel="Iteration", ylabel="w", yscale=log10)
+scatterlines!(ax_w, 1:length(solution.info[:accept]), solution.info[:w], color=colors_accept, marker=:circle, markersize=7)
+
+ax_J = Axis(fig[1,3]; xlabel="Iteration", ylabel="ΔJ", yscale=log10)
+scatterlines!(ax_J, 1:length(solution.info[:accept]), abs.(solution.info[:ΔJ]), color=colors_accept, marker=:circle, markersize=7)
+
+ax_Δ = Axis(fig[2,3]; xlabel="Iteration", ylabel="trust region radius", yscale=log10)
+scatterlines!(ax_Δ, 1:length(solution.info[:accept]), solution.info[:Δ], color=colors_accept, marker=:circle, markersize=7)
 
 display(fig)
 println("Done!")
