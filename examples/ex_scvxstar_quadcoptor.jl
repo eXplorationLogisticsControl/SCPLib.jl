8591ec9c22c9d6d74b95726ee8024e97b1c9b062
Yuri <yuri.shimane@gmail.com>
2025-07-12 17:36:11 -0400

    Moved example to examples directory

--- examples/ex_scvxstar_quadcoptor.jl
+++ examples/ex_scvxstar_quadcoptor.jl
@@ -21,7 +21,7 @@ m = 0.3;                        # kg, mass of quadrotor
 T_min = 1.0;                    # N, minimum thrust
 T_max = 4.0;                    # N, maximum thrust
 theta_max = pi/4;               # rad, maximum tilt angle
-Nseg = 30;                      # number of segments
+N = 30;                         # number of nodes
 
 # initial and final states
 x_initial = [0, 0, 0, 0, 0.5, 0];
@@ -35,7 +35,7 @@ p_obstacle_2 = [0, 7, -0.45]    # m, position of obstacle 2
 
 # ODE parameters
 mutable struct QuadroptorParams
-    u::Vector{Float64}
+    u::Vector
 end
 
 params = QuadroptorParams(zeros(nu))
@@ -58,30 +58,28 @@ function quadrotor_rhs!(dx, x, p, t)
     dx[1:3] = x[4:6]
     dx[4:6] = -k_D*norm(x[4:6])*x[4:6] + g
     B = quadrotor_dfdu(x[1:6], p.u, p, t)
-    dx[:] += B * p.u
+    dx[1:6] += B * p.u
     return
 end
 
+# function quadroptor_rhs_aug!(dx_aug, x_aug, p, t)
+#     quadrotor_rhs!(dx_aug, x_aug, p, t)
 
-function quadroptor_rhs_aug!(dx_aug, x_aug, p, t)
-    A = quadrotor_dfdx(x_aug[1:6], p.u, p, t)
-    B = quadrotor_dfdu(x_aug[1:6], p.u, p, t)
+#     # derivatives of Phi_A, Phi_B
+#     A = quadrotor_dfdx(x_aug[1:6], p.u, p, t)
+#     B = quadrotor_dfdu(x_aug[1:6], p.u, p, t)
+#     dx_aug[7:42] = reshape((A * reshape(x_aug[7:42],6,6)')', 36)
+#     dx_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu] = reshape((A * reshape(x_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu], (nu,nx))' + B)', nx*nu)
+# end
 
-    dx_aug[1:3] = x_aug[4:6]
-    dx_aug[4:6] = -k_D*norm(x_aug[4:6])*x_aug[4:6] + g
-    dx_aug[1:6] += B * p.u
-
-    # derivatives of Phi_A, Phi_B
-    dx_aug[7:42] = reshape((A * reshape(x_aug[7:42],6,6)')', 36)
-    dx_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu] = reshape((A * reshape(x_aug[nx*(nx+1)+1:nx*(nx+1)+nx*nu], (nu,nx))' + B)', nx*nu)
-end
+eom_aug! = SCPLib.get_continuous_augmented_eom(quadrotor_rhs!, params, nx)
 
 # -------------------- define objective & non-convex constraints -------------------- #
 function objective(x, u, y)
     return sum(u[4,:])
 end
 
-nh = 2 * prob.N    # two obstacles, enforced at each node
+nh = 2 * N    # two obstacles, enforced at each node
 function h_noncvx(x,u,y)
     h = vcat(
         [R_obstacle_1 - norm(x[1:3,k] - p_obstacle_1) for k in 1:N],
@@ -91,7 +89,6 @@ function h_noncvx(x,u,y)
 end
 
 # -------------------- create problem -------------------- #
-N = 30
 times = LinRange(0.0, t_N, N)
 
 x_ref = hcat([[el for el in LinRange(x_initial[i], x_final[i], N)] for i in 1:6]...)'
@@ -112,7 +109,7 @@ prob = SCPLib.ContinuousProblem(
     y_ref;
     nh = nh,
     h_noncvx = h_noncvx,
-    eom_aug! = quadroptor_rhs_aug!,
+    #eom_aug! = quadroptor_rhs_aug!,
     ode_method = Tsit5(),
 )
 set_silent(prob.model)
@@ -141,6 +138,8 @@ algo = SCPLib.SCvxStar(nx, N; nh=nh, w0 = 10.0)   # don't forget to pass `nh` to
 # solve problem
 solution = SCPLib.solve!(algo, prob, x_ref, u_ref, y_ref; tol_opt = 1e-6, tol_feas = 1e-6)
 
+
+# -------------------- analysis of solution -------------------- #
 # propagate solution
 sols_opt, g_dynamics_opt = SCPLib.get_trajectory(prob, solution.x, solution.u, solution.y)
 
