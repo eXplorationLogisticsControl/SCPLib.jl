151cf4ccf38bb963b76eedfe60f0e3960651fdaf
Yuri <yuri.shimane@gmail.com>
2026-01-24 18:44:50 -0800

    Added option with fixed trw algorithm

--- examples_astrodynamics/advanced_dionysus.jl
+++ examples_astrodynamics/advanced_dionysus.jl
@@ -118,6 +118,7 @@ prob = SCPLib.ContinuousProblem(
     u_ref;
     ng = ng,
     g_noncvx = g_noncvx,
+    ode_ensemble_method = EnsembleThreads(),
     ode_method = Vern7(),
 )
 set_silent(prob.model)
@@ -142,14 +143,16 @@ sols_ig, _ = SCPLib.get_trajectory(prob, x_ref, u_ref)
 # -------------------- instantiate algorithm -------------------- #
 algo = SCPLib.SCvxStar(nx, N; ng=ng, w0 = 1e0, Δ0=0.1, w_max=1e20)  # known to work: w0 = 1e0 with N = 500
 
+# algo = SCPLib.FixedTRWSCP(nx, N, 0.05)
 # w_ep = 1e2
 # w_prox = 1e1
 # algo = SCPLib.ProxLinear(w_ep, w_prox)
 
 # solve problem
-tol_feas = 1e-8
+maxiter = 1000
+tol_feas = 1e-6
 tol_opt = 1e-6
-solution = SCPLib.solve!(algo, prob, x_ref, u_ref; tol_feas = tol_feas, tol_opt = tol_opt, maxiter = 500)
+solution = SCPLib.solve!(algo, prob, x_ref, u_ref; tol_feas = tol_feas, tol_opt = tol_opt, maxiter = maxiter)
 sols_opt, g_dynamics_opt = SCPLib.get_trajectory(prob, solution.x, solution.u)
 @show -solution.info[:J0][end] * MASS
 
