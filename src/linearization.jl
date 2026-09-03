c7f8a9df0fef71da8df3ca8a6b925d310dbd46ee
Yuri <yuri.shimane@gmail.com>
2026-09-03 18:38:19 +0900

    Fix for `EnsembleSolution` import to be via `SciMLBase`

--- src/linearization.jl
+++ src/linearization.jl
@@ -23,7 +23,7 @@ end
 
 
 """Set cache for continuous dynamics"""
-function set_continuous_dynamics_cache!(lincache::MultipleShootingCache, x_ref, u_ref, sols::Union{Vector{ODESolution},EnsembleSolution})
+function set_continuous_dynamics_cache!(lincache::MultipleShootingCache, x_ref, u_ref, sols::Union{Vector{ODESolution},SciMLBase.EnsembleSolution})
     nx, N = size(x_ref)
     nu, _ = size(u_ref)
     for (k,sol) in enumerate(sols)
@@ -36,7 +36,7 @@ end
 
 
 """Set cache for impulsive dynamics"""
-function set_impulsive_dynamics_cache!(lincache::MultipleShootingCache, x_ref, u_ref, sols::Union{Vector{ODESolution},EnsembleSolution}, dfdu::Function)
+function set_impulsive_dynamics_cache!(lincache::MultipleShootingCache, x_ref, u_ref, sols::Union{Vector{ODESolution},SciMLBase.EnsembleSolution}, dfdu::Function)
     nx, N = size(x_ref)
     for (k,sol) in enumerate(sols)
         xf_aug = sol.u[end]
