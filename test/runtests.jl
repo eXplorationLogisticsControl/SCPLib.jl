e59ce77b33ec985e9dc7f193276c3fb2b5c1b113
Yuri <yuri.shimane@gmail.com>
2026-09-22 23:29:11 -0700

    Remove excess import

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,7 +22,6 @@ get_plot = false
 
     include("test_scvxstar_forwardbackward.jl")
     include("test_scvxstar_callback.jl")
-    include("test_scvxstar_forwardbackward.jl")
 end
 
 @testset "Biased control" begin
